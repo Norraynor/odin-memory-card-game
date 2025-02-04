05bc9dd8a8138565154948302d3700a0e45d9b2d
norraynor <37270508+Norraynor@users.noreply.github.com>
2025-02-04 21:08:34 +0100

    Add static cards info in array

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,63 @@ import Game from "./Game";
 
 function App() {
 	const [score, setScore] = useState(0);
-	const cardsArray = new Array(10);
+	// const cardsArray = new Array(10);
+	const pokeArray = [
+		{
+			name: "bulbasaur",
+			sprite:
+				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+		},
+		{
+			name: "charmander",
+			sprite:
+				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+		},
+		{
+			name: "squirtle",
+			sprite:
+				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+		},
+		{
+			name: "pidgey",
+			sprite:
+				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png",
+		},
+		{
+			name: "gyarados",
+			sprite:
+				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/130.png",
+		},
+		{
+			name: "magikarp",
+			sprite:
+				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/129.png",
+		},
+		{
+			name: "pikachu",
+			sprite:
+				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+		},
+		{
+			name: "zapdos",
+			sprite:
+				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/145.png",
+		},
+		{
+			name: "moltres",
+			sprite:
+				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/146.png",
+		},
+		{
+			name: "articuno",
+			sprite:
+				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/144.png",
+		},
+	];
 	return (
 		<>
 			<TopBar getScore={score} />
-			<Game cards={cardsArray} getScore={score} setScore={setScore} />
+			<Game cards={pokeArray} getScore={score} setScore={setScore} />
 		</>
 	);
 }
