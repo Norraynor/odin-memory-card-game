a61c4a48981529c5b2b72bf1bba650f563a8e1e7
norraynor <37270508+Norraynor@users.noreply.github.com>
2025-02-10 21:02:00 +0100

    Add style to topbar

--- src/TopBar.jsx
+++ src/TopBar.jsx
@@ -1,16 +1,17 @@
 /* eslint-disable react/prop-types */
 // game name goes here and score
+import "./TopBar.css";
 function TopBar(props) {
 	return (
 		<>
 			<div className="top-bar">
-				<div className="right">
+				<div className="left">
 					<div className="name">Pokemon Memory Game</div>
 					<div className="rules">
-						Click cards that haven`t been clicked yet for score
+						Click cards that haven`t been clicked in this round for score
 					</div>
 				</div>
-				<div className="left">
+				<div className="right">
 					<div className="score">Score: {props.getScore}</div>
 					<div className="best">Best Score: {props.getBestScore}</div>
 				</div>
